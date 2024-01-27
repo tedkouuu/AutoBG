dbb9e997f7a475673d2922678bf301bc9994fff9
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2024-01-28 00:37:21 +0200

    [Add] Added Offer entity

---
+++ src/main/java/project/autobg/model/entity/OfferEntity.java
@@ -0,0 +1,121 @@
+package project.autobg.model.entity;
+
+import jakarta.persistence.*;
+import project.autobg.model.enums.EngineEnum;
+import project.autobg.model.enums.TransmissionEnum;
+
+import java.math.BigDecimal;
+
+@Table(name = "offers")
+@Entity
+public class OfferEntity extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EngineEnum engine;
+
+    private String imageUrl;
+
+    private int mileage;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransmissionEnum transmission;
+
+    private int year;
+
+    @ManyToOne
+    private ModelEntity model;
+
+    @ManyToOne
+    private UserEntity seller;
+
+    public EngineEnum getEngine() {
+        return engine;
+    }
+
+    public OfferEntity setEngine(EngineEnum engine) {
+        this.engine = engine;
+        return this;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public OfferEntity setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+        return this;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public OfferEntity setMileage(int mileage) {
+        this.mileage = mileage;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public OfferEntity setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public TransmissionEnum getTransmission() {
+        return transmission;
+    }
+
+    public OfferEntity setTransmission(TransmissionEnum transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public OfferEntity setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public ModelEntity getModel() {
+        return model;
+    }
+
+    public OfferEntity setModel(ModelEntity model) {
+        this.model = model;
+        return this;
+    }
+
+    public UserEntity getSeller() {
+        return seller;
+    }
+
+    public OfferEntity setSeller(UserEntity seller) {
+        this.seller = seller;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "OfferEntity{" +
+                "engine=" + engine +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", mileage=" + mileage +
+                ", price=" + price +
+                ", transmission=" + transmission +
+                ", year=" + year +
+                ", model=" + model +
+                ", seller=" + seller +
+                '}';
+    }
+}
---
+++ src/main/java/project/autobg/model/enums/EngineEnum.java
@@ -0,0 +1,8 @@
+package project.autobg.model.enums;
+
+public enum EngineEnum {
+    GASOLINE,
+    DIESEL,
+    ELECTRIC,
+    HYBRID
+}
