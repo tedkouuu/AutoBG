e78cfb41a262a065cd9964efca562805a18ce0b6
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2024-02-06 16:51:33 +0200

    [Add] Implementing the filter chain

--- src/main/java/project/autobg/config/SecurityConfig.java
+++ src/main/java/project/autobg/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package project.autobg.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import project.autobg.model.enums.UserRoleEnum;
 import project.autobg.repository.UserRepository;
 import project.autobg.service.AutoBGUserDetailsService;
 
@@ -19,9 +22,26 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
-        return null;
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity.authorizeHttpRequests(
+                authorizeRequests -> authorizeRequests
+                        // Allow access to static resources at common locations
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        // Allow access to any endpoint without authentication
+                        .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                        // Allow access to specific paths without authentication
+                        .requestMatchers("/", "/users/login", "/users/register").permitAll()
+                        // Require the role of MODERATOR for access to "/pages/moderators"
+                        .requestMatchers("/pages/moderators").hasRole(UserRoleEnum.MODERATOR.name())
+                        // Require the role of ADMIN for access to "/pages/admins"
+                        .requestMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name())
+                        .requestMatchers("/support").authenticated()
+                        // Require authentication for any other request
+                        .anyRequest().authenticated()).build();
+
     }
+
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
---
+++ src/main/java/project/autobg/web/PagesController.java
@@ -0,0 +1,23 @@
+package project.autobg.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PagesController {
+
+    @GetMapping("/pages/all")
+    public String all() {
+        return "all";
+    }
+
+    @GetMapping("/pages/moderators")
+    public String moderators() {
+        return "moderators";
+    }
+
+    @GetMapping("/pages/admins")
+    public String admins() {
+        return "admins";
+    }
+}
