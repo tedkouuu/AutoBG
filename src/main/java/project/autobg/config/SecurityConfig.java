2526a48cb4461f4ad448cbbb28804b72a3f59850
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2024-02-06 15:58:50 +0200

    [Add] Added Spring security

--- src/main/java/project/autobg/config/SecurityConfig.java
+++ src/main/java/project/autobg/config/SecurityConfig.java
@@ -2,8 +2,13 @@ package project.autobg.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import project.autobg.repository.UserRepository;
+import project.autobg.service.AutoBGUserDetailsService;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +16,16 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+        return null;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new AutoBGUserDetailsService(userRepository);
+
     }
 }
---
+++ src/main/java/project/autobg/service/AutoBGUserDetailsService.java
@@ -0,0 +1,45 @@
+package project.autobg.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import project.autobg.model.entity.UserEntity;
+import project.autobg.model.entity.UserRoleEntity;
+import project.autobg.repository.UserRepository;
+
+// NOTE: This is not annotated as @Service, because I will return it as a bean
+public class AutoBGUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AutoBGUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.
+                findByEmail(username).
+                map(this::map).
+                orElseThrow(() -> new UsernameNotFoundException("User with email " + username + " not found!"));
+    }
+
+    private UserDetails map(UserEntity userEntity) {
+        return User.builder().
+                username(userEntity.getEmail()).
+                password(userEntity.getPassword()).
+                authorities(userEntity.getUserRoles().
+                        stream().
+                        map(this::map).
+                        toList()).build();
+    }
+
+    private GrantedAuthority map(UserRoleEntity userRole) {
+        return new SimpleGrantedAuthority("ROLE_" + userRole.
+                getUserRole().
+                name());
+    }
+}
